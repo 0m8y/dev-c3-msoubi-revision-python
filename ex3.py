0b87a4a90d8b62ba1dcc4b7e26726ff26414f0c5
0m8y <msoubi@icloud.com>
2023-11-14 16:22:14 +0100

    ex3: error handling + minor correction

--- ex3.py
+++ ex3.py
@@ -3,7 +3,7 @@ def multiplication(num1, num2):
     return num1 * num2
 
 def division(num1, num2):
-    if num2 == 0:
+    if num2 == 0.0:
         print("Division par 0 impossible.")
         exit(84)
     return num1 / num2
@@ -15,28 +15,34 @@ def substraction(num1, num2):
     return num1 - num2
 
 def simple_calcul(func, position, calcul_list):
-    calcul_list[position] = str(func(int(calcul_list[position - 1]), int(calcul_list[position + 1])))
+    calcul_list[position] = str(func(float(calcul_list[position - 1]), float(calcul_list[position + 1])))
     calcul_list.pop(position - 1)
-    calcul_list.pop(position + 1)
+    calcul_list.pop(position)
     return calcul_list
 
 def calculator(calcul):
     iterator = 0
     for element in calcul :
         if element == "*" :
-            calculator(simple_calcul(multiplication, iterator, calcul))
+            return calculator(simple_calcul(multiplication, iterator, calcul))
         if element == "/" :
-            calculator(simple_calcul(division, iterator, calcul))
+            return calculator(simple_calcul(division, iterator, calcul))
         iterator += 1
     iterator = 0
+
     for element in calcul :
         if element == "+" :
-            calculator(simple_calcul(addition, iterator, calcul))
+            return calculator(simple_calcul(addition, iterator, calcul))
         if element == "-" :
-            calculator(simple_calcul(substraction, iterator, calcul))
+            return calculator(simple_calcul(substraction, iterator, calcul))
         iterator += 1
+    
+    if len(calcul) == 1 :
+        return calcul[0]
+    else :
+        print("Calcul invalideee")
+        exit(84)
 
-    print("result is " + calcul[0])
 
 def main():
     calcul_input = input("Entrez votre calcul: ")
@@ -44,7 +50,8 @@ def main():
     calcul_regex = re.findall(pattern, calcul_input)
 
     if calcul_regex:
-        calculator(calcul_regex[0])
+        print("result is " + calculator(calcul_regex))
+        
     else:
         print("Calcul invalide")
         exit(84)
