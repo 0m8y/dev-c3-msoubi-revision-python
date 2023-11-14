493aa948e6657f7340f80fb94f13be295ef7b89b
0m8y <msoubi@icloud.com>
2023-11-14 14:59:54 +0100

    ex2: lower + nombres de caractères

--- ex2.py
+++ ex2.py
@@ -2,6 +2,9 @@
 def main():
     phrase = input("Entrez une phrase: ")
     print(phrase.upper())
+    print(phrase.lower())
+    print("Nombre de caractère: " + str(len(phrase)))
+
 
 if __name__ == "__main__":
     main()
